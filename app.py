d78fabdb95f8215c0c15cab1c4d9d9ead3413249
kaiser24 <damsog38@gmail.com>
2021-11-17 23:17:20 -0500

    Working on the python API. encode_images calls the encoder and returns the embedding

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, Request, Response
 import json, os, time
 from uuid import uuid4
+
+from flask.globals import request
 import encoder_extractor
 
 # Some definitions
@@ -22,31 +24,40 @@ encoder = encoder_extractor.encoderExtractor(None)
 
 @app.route('/load_models', methods=['GET'])
 def load_models():
+    result = "0"
     print("load_models")
     return result
 
 @app.route('/unload_models', methods=['GET'])
 def unload_models():
+    result = "0"
     print("unload_models")
     return result
 
 @app.route('/encode_images', methods=['POST'])
 def encode_images():
+    result = "0"
     print("encode_images")
-    return result
+    encoder.set_input_data( str(request.get_json("imgs")).replace("'",'"') )
+    result = encoder.process_data()
+    
+    return str(result)
 
 @app.route('/compare_to_dataset', methods=['POST'])
 def compare_to_dataset():
+    result = "0"
     print("compare_to_dataset")
     return result
 
 @app.route('/start_live_analytics', methods=['GET'])
 def start_live_analytics():
+    result = "0"
     print("start_live_analytics")
     return result
 
 @app.route('/stop_live_analytics', methods=['GET'])
 def stop_live_analytics():
+    result = 0
     print("stop_live_analytics")
     return result
 
