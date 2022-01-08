e08a2fd3105dcfbf5721c3382e592e9cdf5db7e0
kaiser24 <damsog38@gmail.com>
2022-01-08 16:05:28 -0500

    Extended the correct input options from analyzer to processor and added the detect_image and detect_video methods tos processor

--- videoAnalytics/processor.py
+++ videoAnalytics/processor.py
@@ -48,6 +48,14 @@ class processor:
         else:
             result = self.analyzer.analyze_img(image, return_img, return_landmarks, return_img_json)
             return result
+    
+    def detect_image(self, image, return_img=False, return_landmarks=False, return_img_json=False):
+        if return_img:
+            result,img = self.analyzer.detect_img(image, return_img, return_landmarks, return_img_json)
+            return result, img
+        else:
+            result = self.analyzer.detect_img(image, return_img, return_landmarks, return_img_json)
+            return result
 
     # TODO: Work on video feed to frontend
     def analyze_video(self, dataset_path, video_source, return_img=False, return_landmarks=False, return_img_json=False, show_video=False):
@@ -65,23 +73,43 @@ class processor:
                 fin = cv2.waitKey(1) & 0xFF
                 if(fin == ord('q')):
                     break
+    
+    # TODO: Work on video feed to frontend
+    def detect_video(self, video_source, return_img=False, return_landmarks=False, return_img_json=False, show_video=False):
+        cap = cv2.VideoCapture(video_source)
+
+        while cap.isOpened():
+            ret, img_read = cap.read()
+            result,img = self.analyzer.detect_img(img_read, True, return_landmarks, return_img_json)
+
+            if show_video:
+                cv2.namedWindow('Frame')
+                cv2.imshow('Frame', img)
+
+                fin = cv2.waitKey(1) & 0xFF
+                if(fin == ord('q')):
+                    break
 
 def main() -> None:
     ap = argparse.ArgumentParser()
+    ap.add_argument("type", choices=["detection", "recognition"], help="Select detection for Plain FaceDet and Recognition for FaceDetection and Recognition.")
     ap.add_argument("-i", "--image", required=False, help="Reads an image from path. If not given, opens camera")
     ap.add_argument("-d", "--dataset", required=False, help="path to the dataset json file containing refference info")
-    ap.add_argument("-p", "--show", required=False, help="Prints output on console and shows image result", default=True)
-    ap.add_argument("-t", "--video_test", required=False, help="If selected, opens camera to live test", default=False)
+    ap.add_argument("-p", "--print_output", action="store_true", help="Prints output on console and shows image result")
+    ap.add_argument("-t", "--video_test", action="store_true", help="If selected, opens camera to live test")
     args = vars(ap.parse_args())
+
+    if not args["dataset"]:
+       if args["type"] == "recognition":
+           print("For recognition you must provide a dataset. Use the flag: -d /path/to/dataset")
+           sys.exit()
 
     mProcessor = processor()
 
     # Getting input image. -i to get it from path. else get it from camera
     if args["image"]:
-        print("hereeee")
         input_img = cv2.imread(args['image'])
     else:
-        print("hereeee")
         cap = cv2.VideoCapture(0)
         ret, input_img = cap.read()
         cap.release()
@@ -90,17 +118,24 @@ def main() -> None:
     dataset_path = args["dataset"]
 
     #input_img = cv2.imread('/mnt/72086E48086E0C03/Projects/Face_Recognizer_Service/imgs/00000002.jpg')
-    #-d '/mnt/72086E48086E0C03/Projects/VideoAnalytics_Server/resources/user_data/1/g1/g1embeddings.json' -t true
+    #-d '/mnt/72086E48086E0C03/Projects/VideoAnalytics_Server/resources/user_data/1/g1/g1embeddings.json' -t
 
-    result,img = mProcessor.analyze_image(dataset_path, input_img, return_img=True)
-    if args["show"]:
+    if args["type"] == "recognition":
+        result,img = mProcessor.analyze_image(dataset_path, input_img, return_img=True)
+    else:
+        result,img = mProcessor.detect_image(input_img, return_img=True)
+
+    if args["print_output"]:
         cv2.imshow('image', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     # Video Testing if option selected. stop it pressing q
-    if args["video_test"] == "true":
-        mProcessor.analyze_video(dataset_path, 0, show_video=True)
+    if args["video_test"]:
+        if args["type"] == "recognition":
+            mProcessor.analyze_video(dataset_path, 0, show_video=True)
+        else:
+            mProcessor.detect_video(0, show_video=True )
 
 if __name__=="__main__":
     main()
