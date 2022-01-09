b4f9f3a7e93ec3d11dda1d8161105e1cb773a7a5
kaiser24 <damsog38@gmail.com>
2022-01-09 18:22:37 -0500

    Added a logger and changed logger lever printing

--- videoAnalytics/processor.py
+++ videoAnalytics/processor.py
@@ -4,6 +4,7 @@ import base64
 import argparse
 import insightface
 import numpy as np
+import logging
 from pathlib import Path
 path_root = Path(__file__).parents[1]
 sys.path.append(str(path_root))
@@ -11,13 +12,13 @@ from videoAnalytics.encoder import encoderExtractor
 from videoAnalytics.analyzer import faceAnalyzer
 
 class processor:
-    def __init__(self) -> None:
+    def __init__(self, face_detection_model='retinaface_r50_v1', face_recognition_model='arcface_r100_v1') -> None:
         #loading the face detection model. 0 means to work with GPU. -1 is for CPU.
-        detector = insightface.model_zoo.get_model('retinaface_r50_v1')
+        detector = insightface.model_zoo.get_model(face_detection_model)
         detector.prepare(ctx_id = 0, nms=0.4)
 
         #loading the face recognition model. 0 means to work with GPU. -1 is for CPU.
-        recognizer = insightface.model_zoo.get_model('arcface_r100_v1')
+        recognizer = insightface.model_zoo.get_model(face_recognition_model)
         recognizer.prepare(ctx_id = 0)
 
         self.encoder = encoderExtractor(None, detector, recognizer)
@@ -95,43 +96,66 @@ def main() -> None:
     ap.add_argument("type", choices=["detection", "recognition"], help="Select detection for Plain FaceDet and Recognition for FaceDetection and Recognition.")
     ap.add_argument("-i", "--image", required=False, help="Reads an image from path. If not given, opens camera")
     ap.add_argument("-d", "--dataset", required=False, help="path to the dataset json file containing refference info")
-    ap.add_argument("-p", "--print_output", action="store_true", help="Prints output on console and shows image result")
     ap.add_argument("-t", "--video_test", action="store_true", help="If selected, opens camera to live test")
+    ap.add_argument("-v", "--verbose", action="store_true", help="Debug level for logger output")
+    ap.add_argument("-s", "--show_img", action="store_true", help="Shows image output")
     args = vars(ap.parse_args())
+
+    # Logger Configuration
+    logger = logging.getLogger(__name__)
+    logger_format = '%(asctime)s:%(name)s:%(levelname)s:%(message)s'
+    logger_date_format = '[%Y/%m/%d %H:%M:%S]'
+
+    if args["verbose"]:
+        logging.basicConfig(level=logging.DEBUG, format=logger_format, datefmt=logger_date_format)
+    else:
+        logging.basicConfig(level=logging.INFO,  format=logger_format, datefmt=logger_date_format)
 
     if not args["dataset"]:
        if args["type"] == "recognition":
-           print("For recognition you must provide a dataset. Use the flag: -d /path/to/dataset")
+           logger.info("For recognition you must provide a dataset. Use the flag: -d /path/to/dataset")
            sys.exit()
 
-    mProcessor = processor()
+    logger.info("Preparing the models")
+    face_detection_model = 'retinaface_r50_v1'
+    face_recognition_model = 'arcface_r100_v1'
+    logger.debug(f'Face detection model: {face_detection_model}')
+    logger.debug(f'Face recognition model: {face_recognition_model}')
+    mProcessor = processor(face_detection_model,face_recognition_model)
 
     # Getting input image. -i to get it from path. else get it from camera
     if args["image"]:
+        logger.info("Reading image from path")
         input_img = cv2.imread(args['image'])
     else:
+        logger.info("Image not provided. reading camera instead")
         cap = cv2.VideoCapture(0)
         ret, input_img = cap.read()
         cap.release()
         pass
     
+    logger.info(f'Reading dataset from path {args["dataset"]}')
     dataset_path = args["dataset"]
 
     #input_img = cv2.imread('/mnt/72086E48086E0C03/Projects/Face_Recognizer_Service/imgs/00000002.jpg')
     #-d '/mnt/72086E48086E0C03/Projects/VideoAnalytics_Server/resources/user_data/1/g1/g1embeddings.json' -t
 
     if args["type"] == "recognition":
+        logger.debug("Performing face recognition on image")
         result,img = mProcessor.analyze_image(dataset_path, input_img, return_img=True)
     else:
+        logger.debug("Performing face detection on image")
         result,img = mProcessor.detect_image(input_img, return_img=True)
 
-    if args["print_output"]:
+    if args["show_img"]:
+        logger.info("Image processed will be shown")
         cv2.imshow('image', img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
     # Video Testing if option selected. stop it pressing q
     if args["video_test"]:
+        logger.info("Starting video test")
         if args["type"] == "recognition":
             mProcessor.analyze_video(dataset_path, 0, show_video=True)
         else:
