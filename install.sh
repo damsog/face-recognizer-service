00fa9fa74c8d04ef7209dec98392f1ce3ed23b0e
kaiser24 <damsog38@gmail.com>
2022-11-14 17:20:07 -0500

    Added out folder if out to save output encoddings is selected

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.7
 #python3 -m pip install virtualenv
-virtualenv face_videoanalytics_venv --python=python3.7
+python3.7 -m virtualenv face_videoanalytics_venv
 source face_videoanalytics_venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install virtualenv
