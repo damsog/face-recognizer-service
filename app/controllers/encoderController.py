c96b90b874115e83b942a6b56e5c2d0759ef08b2
kaiser24 <damsog38@gmail.com>
2022-11-26 16:37:41 -0500

    Finished endpoint to process multiple images

--- app/controllers/encoderController.py
+++ app/controllers/encoderController.py
@@ -2,35 +2,40 @@ from typing import List
 import cv2
 from fastapi import APIRouter, Form, Request, Response, status, File, UploadFile
 
-from ..libs.imageHelper import ImageHelper
-from ..services.encoderService import EncoderService
+from app.libs.imageHelper import ImageHelper
+from app.services.encoderService import EncoderService
 import numpy as np
+
+from videoAnalytics.encoder import OutputData
 
 router = APIRouter(prefix="/encoder", tags=["Encoder"])
 
 @router.post("/image")
-async def encode(request: Request, response: Response, file: UploadFile = File(...)):
+async def encode(request: Request, response: Response, file: UploadFile = File(...)) -> List[float]:
     try:
         # Reading the image
-        contents = await file.read()
+        file_content = await file.read()
 
-        img = ImageHelper.multipart_to_image(contents)
+        img = ImageHelper.multipart_to_image(file_content)
 
-        result = await EncoderService.encode(request.app.state.processor, img)
+        result = await EncoderService.encode_image(request.app.state.processor, img)
         return result
     except Exception as e:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"error": str(e)}
 
 @router.post("/images")
-async def encode(request: Request, response: Response, key: str = Form(...), file: List[UploadFile] = File(...)):
+async def encode(request: Request, response: Response, key: str = Form(...), files: List[UploadFile] = File(...)) -> OutputData:
     try:
-        # Reading the image
-        contents = await file.read()
+        # Reading the images
+        files_content = []
+        for file in files:
+            file_content = await file.read()
+            files_content.append(file_content)
 
-        img = ImageHelper.multipart_to_image(contents)
+        imgs = ImageHelper.multipart_to_image_list(files_content)
 
-        result = await EncoderService.encode(request.app.state.processor, img)
+        result = await EncoderService.encode_images(request.app.state.processor, imgs, key)
         return result
     except Exception as e:
         response.status_code = status.HTTP_400_BAD_REQUEST
