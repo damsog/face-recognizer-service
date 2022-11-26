529b3dff7529083a975ac46e5fcf4b19e5d2b231
kaiser24 <damsog38@gmail.com>
2022-11-26 15:53:50 -0500

    Started a fastapi server. thinking of migrating to fastapi

---
+++ app/configurations/information.py
@@ -0,0 +1,22 @@
+## API Information
+
+title = "Gnosis Recognition API"
+
+description = """
+Gnosis Recognition API allows to apply face Detection. 
+Face Recognition, and encoding (needed for Face Recognition)
+Both on Single images and video feed. 🚀 
+
+## Base API
+
+### API Endpoints
+
+"""
+
+version = "1.0.0"
+
+contact = {
+        "name": "Felipe Serna",
+        "url": "http://github.com/damsog/",
+        "email": "damsog38@gmail.com",
+    }
---
+++ app/main.py
@@ -0,0 +1,33 @@
+import asyncio
+from fastapi import FastAPI
+from .configurations.information import *
+from aiortc.contrib.media import MediaRelay
+from videoAnalytics.processor import processor
+
+from .controllers import encoderController
+
+def create_app():
+    app = FastAPI(
+        title=title,
+        description=description,
+        version=version,
+        contact=contact,
+    )
+
+    app.include_router(encoderController.router)
+
+    @app.on_event("startup")
+    async def startup():
+        app.state.processor = processor()
+        app.state.pcs = set()
+        app.state.relay = MediaRelay()
+
+    @app.on_event("shutdown")
+    async def shutdown():
+        coros = [pc.close() for pc in app.state.pcs]
+        await asyncio.gather(*coros)
+        app.state.pcs.clear()
+
+    return app
+
+app = create_app()
