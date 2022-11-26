af9c5894abfac81169cdb05d20efb26a783b5100
kaiser24 <damsog38@gmail.com>
2022-11-26 16:36:30 -0500

    Fixed processing multiple images service

--- app/services/encoderService.py
+++ app/services/encoderService.py
@@ -1,8 +1,16 @@
 from typing import List
 import numpy as np
 
+from videoAnalytics.encoder import OutputData
+from videoAnalytics.processor import processor
+
 class EncoderService:
     @staticmethod
-    async def encode(processor, img: np.ndarray) -> List[float]:
+    async def encode_image(processor: processor, img: np.ndarray) -> List[float]:
         result = processor.encode_narray_image(img)
+        return result
+
+    @staticmethod
+    async def encode_images(processor: processor, imgs: List[np.ndarray], key: str) -> OutputData:
+        result = processor.encode_narray_images(imgs, key)
         return result
