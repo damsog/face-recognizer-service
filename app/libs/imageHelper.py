71cdd364580909aa237224c6e3606ec59dfb68b6
kaiser24 <damsog38@gmail.com>
2022-11-26 16:37:01 -0500

    Image helper for multiple content bytes to images

--- app/libs/imageHelper.py
+++ app/libs/imageHelper.py
@@ -1,10 +1,18 @@
+from typing import List
 import cv2
 import numpy as np
 
 
 class ImageHelper:  
     @staticmethod
-    def multipart_to_image(multipart):
-        nparr = np.fromstring(multipart, np.uint8)
+    def multipart_to_image(content: bytes) -> np.ndarray:
+        nparr = np.fromstring(content, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         return img
+    
+    @staticmethod
+    def multipart_to_image_list(content_list: List[bytes]) -> List[np.ndarray]:
+        imgs = []
+        for content in content_list:
+            imgs.append(ImageHelper.multipart_to_image(content))
+        return imgs
