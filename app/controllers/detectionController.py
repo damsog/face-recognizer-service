549eecac51e25045904c4c05418cba7240151582
kaiser24 <damsog38@gmail.com>
2022-11-26 23:20:44 -0500

    Added parameter to select if requesting the b64 result image or not

--- app/controllers/detectionController.py
+++ app/controllers/detectionController.py
@@ -6,14 +6,14 @@ from app.services.detectionService import DetectionService
 router = APIRouter(prefix="/detector", tags=["Detector"])
 
 @router.post("/image")
-async def detect(request: Request, response: Response, file: UploadFile = File(...)):
+async def detect(request: Request, response: Response, file: UploadFile = File(...), return_img_b64: bool = False):
     try:
         # Reading the image
         file_content = await file.read()
 
         img = ImageHelper.multipart_to_image(file_content)
 
-        result = await DetectionService.detect_image(request.app.state.processor, img)
+        result = await DetectionService.detect_image(request.app.state.processor, img, return_img_b64)
         return result
     except Exception as e:
         response.status_code = status.HTTP_400_BAD_REQUEST
--- app/services/detectionService.py
+++ app/services/detectionService.py
@@ -8,7 +8,7 @@ from videoAnalytics.processor import processor
 
 class DetectionService:
     @staticmethod
-    async def detect_image(processor: processor, img: ndarray) -> List[float]:
-        result = processor.detect_image(img, return_img_json=True)
+    async def detect_image(processor: processor, img: ndarray, return_img_b64: bool) -> List[float]:
+        result = processor.detect_image(img, return_img_json=return_img_b64)
         return result
     
