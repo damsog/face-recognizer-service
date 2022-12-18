6884ae18d462b46ba3c0a41a7a04aea3b379b19f
kaiser24 <damsog38@gmail.com>
2022-12-17 19:44:32 -0500

    Receiving sdp info in a form for detection as it is for recognition

--- app/controllers/detectionController.py
+++ app/controllers/detectionController.py
@@ -21,13 +21,13 @@ async def detect(request: Request, response: Response, file: UploadFile = File(.
         return {"error": str(e)}
 
 @router.post("/stream")
-async def detect_stream(request: Request, response: Response, pc: PeerConnectionDTO):
+async def detect_stream(request: Request, response: Response, sdp: str = Form(...), sdp_type: str = Form(...)):
     try:
         result = await DetectionService.detect_stream(request.app.state.processor, 
                                                     request.app.state.pcs, 
                                                     request.app.state.relay, 
-                                                    pc.sdp, 
-                                                    pc.sdp_type)
+                                                    sdp, 
+                                                    sdp_type)
         return result
     except Exception as e:
         response.status_code = status.HTTP_400_BAD_REQUEST
